5fc11b828299d7a722fb2f374466f1749edd750a
vvo <vincent.voyer@algolia.com>
2015-03-30 12:00:51 +0200

    FEATURE: Allow .search(q, params, cb) "callback always last"

--- src/algoliasearch.helper.js
+++ src/algoliasearch.helper.js
@@ -44,6 +44,13 @@ AlgoliaSearchHelper.prototype = {
    *  content: the query answer with an extra 'disjunctiveFacets' attribute
    */
   search : function( q, searchCallback, searchParams ) {
+    // handle .search( q, searchCallback, searchParams )
+    if ( typeof searchParams === "function" ) {
+      var tmpCb = searchParams;
+      searchParams = searchCallback;
+      searchCallback = tmpCb;
+    }
+
     this.q = q;
     this.searchCallback = searchCallback;
     this.searchParams = searchParams || {};
