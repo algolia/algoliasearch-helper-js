6d7ae86111b1eef59076a42acac4ca4d68a56975
bobylito <bobylito@gmail.com>
2017-10-18 10:56:18 +0200

    feat(sffv): can override search when using searchForFacetValues

---
+++ test/spec/algoliasearch.helper/searchForFacetValues.js
@@ -0,0 +1,74 @@
+'use strict';
+
+var test = require('tape');
+// var algoliaSearch = require('algoliasearch');
+// var SearchParameters = require('../../../src/SearchParameters');
+
+var algoliasearchHelper = require('../../../index');
+
+test('searchForFacetValues should search for facetValues with the current state', function(t) {
+  var lastParameters = null;
+  var fakeClient = {
+    addAlgoliaAgent: function() {},
+    initIndex: function() {
+      return {
+        searchForFacetValues: function() {
+          lastParameters = arguments;
+          return Promise.resolve({
+          });
+        }
+      };
+    }
+  };
+
+  var helper = algoliasearchHelper(fakeClient, 'index', {
+    highlightPreTag: 'HIGHLIGHT>',
+    highlightPostTag: '<HIGHLIGHT',
+    query: 'iphone'
+  });
+
+  helper.searchForFacetValues('facet', 'query', 75);
+
+  t.equal(lastParameters[0].query, 'iphone');
+  t.equal(lastParameters[0].facetQuery, 'query');
+  t.equal(lastParameters[0].facetName, 'facet');
+  t.equal(lastParameters[0].highlightPreTag, 'HIGHLIGHT>');
+  t.equal(lastParameters[0].highlightPostTag, '<HIGHLIGHT');
+
+  t.end();
+});
+
+test('searchForFacetValues can override the current search state', function(t) {
+  var lastParameters = null;
+  var fakeClient = {
+    addAlgoliaAgent: function() {},
+    initIndex: function() {
+      return {
+        searchForFacetValues: function() {
+          lastParameters = arguments;
+          return Promise.resolve({
+          });
+        }
+      };
+    }
+  };
+
+  var helper = algoliasearchHelper(fakeClient, 'index', {
+    highlightPreTag: 'HIGHLIGHT>',
+    highlightPostTag: '<HIGHLIGHT',
+    query: 'iphone'
+  });
+
+  helper.searchForFacetValues('facet', 'query', 75, {
+    query: undefined,
+    highlightPreTag: 'highlightTag'
+  });
+
+  t.equal(lastParameters[0].query, undefined);
+  t.equal(lastParameters[0].facetQuery, 'query');
+  t.equal(lastParameters[0].facetName, 'facet');
+  t.equal(lastParameters[0].highlightPreTag, 'highlightTag');
+  t.equal(lastParameters[0].highlightPostTag, '<HIGHLIGHT');
+
+  t.end();
+});
