b90db6e487d287881ac86c72b0647a96a2adbc68
bobylito <bobylito@gmail.com>
2017-10-18 17:34:06 +0200

    chore: make more precise test

--- test/spec/algoliasearch.helper/searchForFacetValues.js
+++ test/spec/algoliasearch.helper/searchForFacetValues.js
@@ -38,7 +38,7 @@ test('searchForFacetValues should search for facetValues with the current state'
   t.end();
 });
 
-test('searchForFacetValues can override the current search state', function(t) {
+test.only('searchForFacetValues can override the current search state', function(t) {
   var lastParameters = null;
   var fakeClient = {
     addAlgoliaAgent: function() {},
@@ -64,7 +64,7 @@ test('searchForFacetValues can override the current search state', function(t) {
     highlightPreTag: 'highlightTag'
   });
 
-  t.equal(lastParameters[0].query, undefined);
+  t.notOk(lastParameters[0].hasOwnProperty('query'));
   t.equal(lastParameters[0].facetQuery, 'query');
   t.equal(lastParameters[0].facetName, 'facet');
   t.equal(lastParameters[0].highlightPreTag, 'highlightTag');
