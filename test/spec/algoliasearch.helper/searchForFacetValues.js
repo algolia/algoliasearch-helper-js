2ca85762e8e187cfd7b96740f283e74841a9b5e7
François Chalifour <francois.chalifour@gmail.com>
2018-04-16 13:30:14 +0200

    test(sffv): Test usage of `client.sffv()` over `client.index.sffv()`

--- test/spec/algoliasearch.helper/searchForFacetValues.js
+++ test/spec/algoliasearch.helper/searchForFacetValues.js
@@ -70,3 +70,60 @@ test('searchForFacetValues can override the current search state', function(t) {
 
   t.end();
 });
+
+test('searchForFacetValues calls the client method over the initIndex method', function(t) {
+  t.plan(2);
+
+  var indexSearchForFacetValuesCalled = 0;
+  var clientSearchForFacetValuesCalled = 0;
+
+  var fakeClient = {
+    addAlgoliaAgent: function() {},
+    searchForFacetValues: function() {
+      clientSearchForFacetValuesCalled++;
+      return Promise.resolve({});
+    },
+    initIndex: function() {
+      return {
+        searchForFacetValues: function() {
+          indexSearchForFacetValuesCalled++;
+          return Promise.resolve({});
+        }
+      };
+    }
+  };
+
+  var helper = algoliasearchHelper(fakeClient, 'index');
+
+  helper.searchForFacetValues('facet', 'query', 1).then(function() {
+    t.equal(clientSearchForFacetValuesCalled, 1, 'client called');
+    t.equal(indexSearchForFacetValuesCalled, 0, 'index not called');
+
+    t.end();
+  });
+});
+
+test('searchForFacetValues calls the initIndex method if no client method', function(t) {
+  t.plan(1);
+
+  var indexSearchForFacetValuesCalled = 0;
+
+  var fakeClient = {
+    addAlgoliaAgent: function() {},
+    initIndex: function() {
+      return {
+        searchForFacetValues: function() {
+          indexSearchForFacetValuesCalled++;
+          return Promise.resolve({});
+        }
+      };
+    }
+  };
+
+  var helper = algoliasearchHelper(fakeClient, 'index');
+
+  helper.searchForFacetValues('facet', 'query', 1).then(function() {
+    t.equal(indexSearchForFacetValuesCalled, 1);
+    t.end();
+  });
+});
