c6f882fb3d71de15e60903cf2a5946d3c92eb0dc
Haroen Viaene <hello@haroen.me>
2018-10-05 10:48:39 +0200

    chore: remove zuul & unused(?) things

--- __tests__/run.js
+++ __tests__/run.js
@@ -1,25 +1,25 @@
-'use strict';
+// 'use strict';
 
-var bulk = require('bulk-require');
-var algolia = require('algoliasearch');
+// var bulk = require('bulk-require');
+// var algolia = require('algoliasearch');
 
-bulk(__dirname, ['spec/**/*.js']);
+// bulk(__dirname, ['spec/**/*.js']);
 
-if (process.env.ONLY_UNIT !== 'true' && process.env.INTEGRATION_TEST_API_KEY && process.env.INTEGRATION_TEST_APPID) {
-  // usage: INTEGRATION_TEST_APPID=$APPID INTEGRATION_TEST_API_KEY=$APIKEY npm run dev
-  bulk(__dirname, ['integration-spec/**/*.js']);
+// if (process.env.ONLY_UNIT !== 'true' && process.env.INTEGRATION_TEST_API_KEY && process.env.INTEGRATION_TEST_APPID) {
+//   // usage: INTEGRATION_TEST_APPID=$APPID INTEGRATION_TEST_API_KEY=$APIKEY npm run dev
+//   bulk(__dirname, ['integration-spec/**/*.js']);
 
-  test.onFinish(cleanupIntegration);
-}
+//   test.onFinish(cleanupIntegration);
+// }
 
-function cleanupIntegration() {
-  console.log('Deleting all indices');
-  var client = algolia(process.env.INTEGRATION_TEST_APPID, process.env.INTEGRATION_TEST_API_KEY);
-  var maybeIndices = client.listIndexes();
-  maybeIndices.then(content => {
-    content.items
-      .map(i => i.name)
-      .filter(n => n.indexOf('_travis-algoliasearch-helper') !== -1)
-      .forEach(n => client.deleteIndex(n));
-  });
-}
+// function cleanupIntegration() {
+//   console.log('Deleting all indices');
+//   var client = algolia(process.env.INTEGRATION_TEST_APPID, process.env.INTEGRATION_TEST_API_KEY);
+//   var maybeIndices = client.listIndexes();
+//   maybeIndices.then(content => {
+//     content.items
+//       .map(i => i.name)
+//       .filter(n => n.indexOf('_travis-algoliasearch-helper') !== -1)
+//       .forEach(n => client.deleteIndex(n));
+//   });
+// }
