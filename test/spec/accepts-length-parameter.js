70f7d232b55fb3a51c4bc6839f152dfe30e8917f
Vaillant Samuel <samuel.vllnt@gmail.com>
2019-04-24 16:42:09 +0200

    chore: move spec to Jest

--- test/spec/accepts-length-parameter.js
+++ test/spec/accepts-length-parameter.js
@@ -1,34 +1,27 @@
 'use strict';
 
-var test = require('tape');
-
 // This test ensures that the helper accepts the `length` (offset/length API param) parameter
 // At some point we were badly iterating over the SearchParameters instance and failed when
 // the `length` parameter was given.
 
-test('helper accepts length parameter', function(t) {
-  t.plan(2);
+var algoliasearchHelper = require('../../');
 
-  var algoliasearch = require('algoliasearch');
-  var sinon = require('sinon');
-  var algoliasearchHelper = require('../../');
+test('helper accepts length parameter', function() {
+  var fakeClient = {
+    search: jest.fn(function() {
+      return new Promise(function() {});
+    })
+  };
 
-  var appId = 'accepts-length-parameter-appId';
-  var apiKey = 'accepts-length-parameter-apiKey';
-  var indexName = 'accepts-length-parameter-indexName';
-
-  var client = algoliasearch(appId, apiKey);
-  var helper = algoliasearchHelper(client, indexName, {
+  var helper = algoliasearchHelper(fakeClient, 'index', {
     length: 2,
     hitsPerPage: 10
   });
 
-  client.search = sinon.stub().returns(new Promise(function() {}));
-
   helper.setQuery('a').search();
 
-  var searchParams = client.search.getCall(0).args[0][0].params;
+  var lastArguments = fakeClient.search.mock.calls[0][0][0];
 
-  t.equal(searchParams.length, 2, 'searchParams.length was set');
-  t.equal(searchParams.hitsPerPage, 10, 'searchParams.hitsPerPage was also set');
+  expect(lastArguments.params.length).toBe(2);
+  expect(lastArguments.params.hitsPerPage).toBe(10);
 });
