42c11d0a51544eea9d149e94fd5c47b1a8114598
Haroen Viaene <hello@haroen.me>
2019-05-06 17:53:50 +0200

    test: adds tests & safe-guard for non-array

--- src/functions/findIndex.js
+++ src/functions/findIndex.js
@@ -2,6 +2,10 @@
 
 // @MAJOR can be replaced by native Array#findIndex when we change support
 module.exports = function find(array, comparator) {
+  if (!Array.isArray(array)) {
+    return -1;
+  }
+
   for (var i = 0; i < array.length; i++) {
     if (comparator(array[i])) {
       return i;
