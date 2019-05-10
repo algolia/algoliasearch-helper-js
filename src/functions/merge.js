04b83b44be7fdb6a097515caee496df0c10c1b1d
Haroen Viaene <hello@haroen.me>
2019-05-10 14:28:31 +0200

    docs(merge): add jsdoc

--- src/functions/merge.js
+++ src/functions/merge.js
@@ -7,7 +7,21 @@ function clone(value) {
   return value;
 }
 
-// NOTE: this behaves like lodash/merge, but does mutate functions if they are a source
+/**
+ * This method is like Object.assign, but recursively merges own and inherited
+ * enumerable keyed properties of source objects into the destination object.
+ *
+ * NOTE: this behaves like lodash/merge, but:
+ * - does mutate functions if they are a source
+ * - treats non-plain objects as plain
+ * - does not work for circular objects
+ * - treats sparse arrays as sparse
+ * - does not convert Array-like objects to arrays
+ *
+ * @param {Object} object The destination object.
+ * @param {...Object} [sources] The source objects.
+ * @returns {Object} Returns `object`.
+ */
 function merge() {
   var sources = Array.prototype.slice.call(arguments);
   var target = sources.shift();
