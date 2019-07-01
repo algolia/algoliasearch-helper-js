9ec5359b972b8846677926e29ed79f5906e5e4fd
Haroen Viaene <hello@haroen.me>
2019-07-01 16:09:09 +0200

    made things required & prettier
    removed duplication

--- index.d.ts
+++ index.d.ts
@@ -13,13 +13,17 @@ import algoliasearch from 'algoliasearch';
 /**
  * The algoliasearchHelper module is the function that will let its
  * contains everything needed to use the Algoliasearch
- * Helper. It is a also a function that instanciate the helper.
+ * Helper. It is a also a function that instantiate the helper.
  * To use the helper, you also need the Algolia JS client v3.
  * @param client an AlgoliaSearch client
  * @param index the name of the index to query
  * @param opts
  */
-declare function algoliasearchHelper(client: Client, index: string, opts: algoliasearchHelper.QueryParameters): algoliasearchHelper.AlgoliaSearchHelper;
+declare function algoliasearchHelper(
+  client: Client,
+  index: string,
+  opts: algoliasearchHelper.QueryParameters
+): algoliasearchHelper.AlgoliaSearchHelper;
 
 declare namespace algoliasearchHelper {
   export const version: string;
@@ -29,12 +33,29 @@ declare namespace algoliasearchHelper {
     lastResults: SearchResults;
     derivedHelpers: AlgoliaSearchHelper[];
 
-    on(event: 'change' | 'search', cb: (state: SearchParameters, lastResults: SearchResults | null) => any): this;
-    on(event: 'searchForFacetValues', cb: (state: SearchParameters, facet: string, query: string) => any): this;
-    on(event: 'searchOnce', cb: (state: SearchParameters) => any): this;
-    on(event: 'result', cb: (results: SearchResults, state: SearchParameters) => any): this;
-    on(event: 'error', cb: (error: any) => any): this;
-    on(event: 'searchQueueEmpty', cb: () => any): this;
+    on(
+      event: 'search',
+      cb: ({ state: SearchParameters, results: SearchResults }) => void
+    ): this;
+    on(
+      event: 'change',
+      cb: ({
+        state: SearchParameters,
+        results: SearchResults,
+        isPageReset: boolean
+      }) => void
+    ): this;
+    on(
+      event: 'searchForFacetValues',
+      cb: ({ state: SearchParameters, facet: string, query: string }) => void
+    ): this;
+    on(event: 'searchOnce', cb: ({ state: SearchParameters }) => void): this;
+    on(
+      event: 'result',
+      cb: ({ results: SearchResults, state: SearchParameters }) => void
+    ): this;
+    on(event: 'error', cb: ({ error: Error }) => void): this;
+    on(event: 'searchQueueEmpty', cb: () => void): this;
 
     /**
      * Start the search with the parameters set in the state. When the
@@ -52,9 +73,8 @@ declare namespace algoliasearchHelper {
     /**
      * Gets the search query parameters that would be sent to the Algolia Client
      * for the hits
-     * @return Query Parameters
      */
-    getQuery(): QueryParameters;
+    getQuery(): algoliasearch.QueryParameters;
 
     /**
      * Start a search using a modified version of the current state. This method does
@@ -92,8 +112,17 @@ declare namespace algoliasearchHelper {
      *   // }
      * }
      */
-    searchOnce(options: QueryParameters): Promise<{ content: SearchResults, state: SearchParameters }>;
-    searchOnce(options: QueryParameters, cb: (error: any, content: SearchResults, state: SearchParameters) => any): undefined;
+    searchOnce(
+      options: QueryParameters
+    ): Promise<{ content: SearchResults; state: SearchParameters }>;
+    searchOnce(
+      options: QueryParameters,
+      cb: (
+        error: Error,
+        content: SearchResults,
+        state: SearchParameters
+      ) => void
+    ): void;
 
     /**
      * Search for facet values based on an query and the name of a faceted attribute. This
@@ -108,7 +137,12 @@ declare namespace algoliasearchHelper {
      * it in the generated query.
      * @return the results of the search
      */
-    searchForFacetValues(facet: string, query: string, maxFacetHits: number, userState: any): Promise<AlgoliaSearchHelper.FacetSearchResult>;
+    searchForFacetValues(
+      facet: string,
+      query: string,
+      maxFacetHits: number,
+      userState: SearchParameters
+    ): Promise<AlgoliaSearchHelper.FacetSearchResult>;
 
     /**
      * Sets the text query used for the search.
@@ -145,7 +179,9 @@ declare namespace algoliasearchHelper {
      * }).search();
      */
     clearRefinements(name?: string): this;
-    clearRefinements(func: (value: any, attribute: string, type: string) => boolean): this;
+    clearRefinements(
+      func: (value: any, attribute: string, type: string) => boolean
+    ): this;
 
     /**
      * Remove all the tag filters.
@@ -290,7 +326,7 @@ declare namespace algoliasearchHelper {
      * Hierarchical facets are a sub type of disjunctive facets that
      * let you filter faceted attributes hierarchically.
      */
-    hierarchicalFacets?: string[] | object[];
+    hierarchicalFacets?: SearchParameters.HierarchicalFacet[];
 
     // Refinements
     /**
@@ -328,7 +364,9 @@ declare namespace algoliasearchHelper {
      * When querying algolia, the values stored in this attribute will
      * be translated into the `facetFilters` attribute.
      */
-    disjunctiveFacetsRefinements?: { [facet: string]: SearchParameters.FacetList };
+    disjunctiveFacetsRefinements?: {
+      [facet: string]: SearchParameters.FacetList;
+    };
     /**
      * This attribute contains all the filters that need to be
      * applied on the numeric attributes.
@@ -360,115 +398,41 @@ declare namespace algoliasearchHelper {
      * When querying algolia, the values stored in this attribute will
      * be translated into the `facetFilters` attribute.
      */
-    hierarchicalFacetsRefinements?: { [facet: string]: SearchParameters.FacetList };
-
-    /**
-     * Contains the optional tag filters in the raw format of the Algolia API.
-     * @see https://www.algolia.com/doc/rest#param-tagFilters
-     */
-    optionalTagFilters?: string;
-
-    /**
-     * Contains the optional facet filters in the raw format of the Algolia API.
-     * @see https://www.algolia.com/doc/rest#param-tagFilters
-     */
-    optionalFacetFilters?: string;
-
-    // Misc. parameters
-    /**
-     * Applies 'exact' on single word queries if the word contains at least 3 characters
-     * and is not a stop word.
-     * Can take two values?: true or false.
-     * By default, its set to false.
-     * @see https://www.algolia.com/doc/rest#param-enableExactOnSingleWordQuery
-     */
-    enableExactOnSingleWordQuery?: boolean;
+    hierarchicalFacetsRefinements?: {
+      [facet: string]: SearchParameters.FacetList;
+    };
   }
 
   type ValueTypes<T> = T extends Array<infer U> ? U : never;
   type ManagedParameters = {
-    [K in ValueTypes<SearchParameters["managedParameters"]>]: SearchParameters[K]
+    [K in ValueTypes<
+      SearchParameters['managedParameters']
+    >]: SearchParameters[K];
   };
 
-  export class SearchParameters implements QueryParameters {
-    index?: string ;
-    disjunctiveFacets?: string[] ;
-    hierarchicalFacets?: string[] | object[] ;
-    facetsRefinements?: { [facet: string]: string[]; } ;
-    facetsExcludes?: { [facet: string]: string[]; } ;
-    disjunctiveFacetsRefinements?: { [facet: string]: string[]; } ;
-    numericRefinements?: { [facet: string]: SearchParameters.OperatorList };
-    tagRefinements?: string[] ;
-    hierarchicalFacetsRefinements?: { [facet: string]: string[]; } ;
-    optionalTagFilters?: string ;
-    optionalFacetFilters?: string ;
-    hitsPerPage?: number ;
-    maxValuesPerFacet?: number ;
-    minWordSizefor1Typo?: number ;
-    minWordSizefor2Typos?: number ;
-    minProximity?: any;
-    allowTyposOnNumericTokens?: boolean ;
-    ignorePlurals?: boolean ;
-    advancedSyntax?: boolean ;
-    analytics?: boolean ;
-    synonyms?: boolean ;
-    replaceSynonymsInHighlight?: boolean ;
-    highlightPreTag?: string ;
-    highlightPostTag?: string ;
-    distinct?: number | boolean ;
-    aroundLatLng?: string ;
-    aroundRadius?: number ;
-    minimumAroundRadius?: number ;
-    aroundPrecision?: number ;
-    snippetEllipsisText?: string;
-    enableExactOnSingleWordQuery?: boolean ;
-    query?: string ;
-    filters?: string ;
-    attributesToRetrieve?: string[] ;
-    restrictSearchableAttributes?: string[] ;
-    facets?: string[] ;
-    facetingAfterDistinct?: boolean ;
-    attributesToHighlight?: string[] ;
-    attributesToSnippet?: string[] ;
-    restrictHighlightAndSnippetArrays?: boolean ;
-    page?: number ;
-    offset?: number ;
-    length?: number ;
-    typoTolerance?: boolean ;
-    disableTypoToleranceOnAttributes?: string[] ;
-    aroundLatLngViaIP?: boolean ;
-    insideBoundingBox?: number[][] ;
-    queryType?: "prefixAll" | "prefixLast" | "prefixNone" ;
-    insidePolygon?: number[][] ;
-    removeWordsIfNoResults?: "none" | "lastWords" | "firstWords" | "allOptional" ;
-    optionalWords?: string[] ;
-    removeStopWords?: boolean | string[] ;
-    disableExactOnAttributes?: string[] ;
-    exactOnSingleWordQuery?: "none" | "attribute" | "word" ;
-    alternativesAsExact?: Array<"ignorePlurals" | "singleWordSynonym" | "multiWordsSynonym"> ;
-    getRankingInfo?: boolean ;
-    numericAttributesToIndex?: string[] ;
-    numericAttributesForFiltering?: string[] ;
-    numericFilters?: string[] ;
-    tagFilters?: string[] ;
-    facetFilters?: string[] | string[][] ;
-    analyticsTags?: string[] ;
-    nbShards?: number ;
-    userData?: string | object ;
-
+  export class SearchParameters implements Required<ManagedParameters> {
     managedParameters: [
       'index',
-      'facets', 'disjunctiveFacets', 'facetsRefinements',
-      'facetsExcludes', 'disjunctiveFacetsRefinements',
-      'numericRefinements', 'tagRefinements', 'hierarchicalFacets', 'hierarchicalFacetsRefinements'
+      'facets',
+      'disjunctiveFacets',
+      'facetsRefinements',
+      'facetsExcludes',
+      'disjunctiveFacetsRefinements',
+      'numericRefinements',
+      'tagRefinements',
+      'hierarchicalFacets',
+      'hierarchicalFacetsRefinements'
     ];
 
-    constructor(newParameters?: QueryParameters)
+    constructor(newParameters?: QueryParameters);
 
     /* Add a disjunctive facet to the disjunctiveFacets attribute of the helper configuration, if it isn't already present. */
     addDisjunctiveFacet(facet: string): SearchParameters;
     /* Adds a refinement on a disjunctive facet. */
-    addDisjunctiveFacetRefinement(facet: string, value: string): SearchParameters;
+    addDisjunctiveFacetRefinement(
+      facet: string,
+      value: string
+    ): SearchParameters;
     /* Exclude a value from a "normal" facet */
     addExcludeRefinement(facet: string, value: string): SearchParameters;
     /* Add a facet to the facets attribute of the helper configuration, if it isn't already present. */
@@ -476,10 +440,21 @@ declare namespace algoliasearchHelper {
     /* Add a refinement on a "normal" facet */
     addFacetRefinement(facet: string, value: string): SearchParameters;
     addHierarchicalFacet(facet: any): SearchParameters;
-    addHierarchicalFacetRefinement(facet: string, path: string): SearchParameters;
-    addNumericRefinement(attribute: string, operator: SearchParameters.Operator, value: number | number[]): SearchParameters;
+    addHierarchicalFacetRefinement(
+      facet: string,
+      path: string
+    ): SearchParameters;
+    addNumericRefinement(
+      attribute: string,
+      operator: SearchParameters.Operator,
+      value: number | number[]
+    ): SearchParameters;
     addTagRefinement(tag: string): SearchParameters;
-    clearRefinements(attribute?: string | ((value: any, attribute: string, type: string) => any)): SearchParameters;
+    clearRefinements(
+      attribute?:
+        | string
+        | ((value: any, attribute: string, type: string) => void)
+    ): SearchParameters;
     clearTags(): SearchParameters;
     filter(filters: string[]): any;
     getConjunctiveRefinements(facetName: string): string[];
@@ -489,7 +464,10 @@ declare namespace algoliasearchHelper {
     getHierarchicalFacetByName(hierarchicalFacetName: string): any;
     getHierarchicalRefinement(facetName: string): string[];
     getNumericRefinements(facetName: string): SearchParameters.OperatorList[];
-    getNumericRefinement(attribute: string, operator: SearchParameters.Operator): Array<number | number[]>;
+    getNumericRefinement(
+      attribute: string,
+      operator: SearchParameters.Operator
+    ): Array<number | number[]>;
     getQueryParams(): Partial<ManagedParameters>;
     getQueryParameter(paramName: string): any;
     getRefinedDisjunctiveFacets(facet: string, value: any): string[];
@@ -502,14 +480,21 @@ declare namespace algoliasearchHelper {
     isFacetRefined(facet: string, value?: string): boolean;
     isHierarchicalFacetRefined(facet: string, value?: string): boolean;
     isHierarchicalFacet(facet: string): boolean;
-    isNumericRefined(attribute: string, operator: SearchParameters.Operator, value?: string): boolean;
+    isNumericRefined(
+      attribute: string,
+      operator: SearchParameters.Operator,
+      value?: string
+    ): boolean;
     isTagRefined(tag: string): boolean;
     static make(newParameters: QueryParameters): SearchParameters;
     removeExcludeRefinement(facet: string, value: string): SearchParameters;
     removeFacet(facet: string): SearchParameters;
     removeFacetRefinement(facet: string, value?: string): SearchParameters;
     removeDisjunctiveFacet(facet: string): SearchParameters;
-    removeDisjunctiveFacetRefinement(facet: string, value?: string): SearchParameters;
+    removeDisjunctiveFacetRefinement(
+      facet: string,
+      value?: string
+    ): SearchParameters;
     removeHierarchicalFacet(facet: string): SearchParameters;
     removeHierarchicalFacetRefinement(facet: string): SearchParameters;
     removeTagRefinement(tag: string): SearchParameters;
@@ -521,21 +506,40 @@ declare namespace algoliasearchHelper {
     setQueryParameter(parameter: string, value: any): SearchParameters;
     setQuery(newQuery: string): SearchParameters;
     setTypoTolerance(typoTolerance: string): SearchParameters;
-    toggleDisjunctiveFacetRefinement(facet: string, value: any): SearchParameters;
+    toggleDisjunctiveFacetRefinement(
+      facet: string,
+      value: any
+    ): SearchParameters;
     toggleExcludeFacetRefinement(facet: string, value: any): SearchParameters;
-    toggleConjunctiveFacetRefinement(facet: string, value: any): SearchParameters;
-    toggleHierarchicalFacetRefinement(facet: string, value: any): SearchParameters;
+    toggleConjunctiveFacetRefinement(
+      facet: string,
+      value: any
+    ): SearchParameters;
+    toggleHierarchicalFacetRefinement(
+      facet: string,
+      value: any
+    ): SearchParameters;
     toggleFacetRefinement(facet: string, value: any): SearchParameters;
     toggleTagRefinement(tag: string): SearchParameters;
-    static validate(currentState: SearchParameters, parameters: QueryParameters): null | Error;
+    static validate(
+      currentState: SearchParameters,
+      parameters: QueryParameters
+    ): null | Error;
   }
 
   namespace SearchParameters {
     type FacetList = string[];
 
-    type OperatorList = {
-      [k in Operator]?: Array<number | number[]>
+    type HierarchicalFacet = {
+      name: string;
+      attributes: string[];
+      separator: string;
     };
+
+    type OperatorList = {
+      [k in Operator]?: Array<number | number[]>;
+    };
+
     type Operator = '=' | '>' | '>=' | '<' | '<=' | '!=';
   }
 
@@ -644,7 +648,7 @@ declare namespace algoliasearchHelper {
     _rawResults: any;
     _state: SearchParameters;
 
-    constructor(state: SearchParameters, results: any[])
+    constructor(state: SearchParameters, results: any[]);
 
     /**
      * Get a facet object with its name
@@ -699,7 +703,10 @@ declare namespace algoliasearchHelper {
      *   });
      * });
      */
-    getFacetValues(attribute: string, opts: any): SearchResults.FacetValue[] | SearchResults.HierarchicalFacet;
+    getFacetValues(
+      attribute: string,
+      opts: any
+    ): SearchResults.FacetValue[] | SearchResults.HierarchicalFacet;
 
     /**
      * Returns the facet stats if attribute is defined and the facet contains some.
@@ -744,7 +751,7 @@ declare namespace algoliasearchHelper {
     }
 
     interface Refinement {
-      type: `numeric` | `facet` | `exclude` | `disjunctive` | `hierarchical`;
+      type: 'numeric' | 'facet' | 'exclude' | 'disjunctive' | 'hierarchical';
       attributeName: string;
       name: string;
       numericValue: number;
@@ -758,3 +765,4 @@ declare namespace algoliasearchHelper {
 }
 
 export = algoliasearchHelper;
+
