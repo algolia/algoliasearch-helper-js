b1dd5239d7ae2a5f736e4a9df607d389aba79120
Haroen Viaene <hello@haroen.me>
2019-07-02 16:56:15 +0200

    chore(dts): remove comment

--- index.d.ts
+++ index.d.ts
@@ -1,10 +1,3 @@
-// Type definitions for algoliasearch-helper 2.26
-// Project: https://community.algolia.com/algoliasearch-helper-js/
-// Definitions by: Gordon Burgett <https://github.com/gburgett>
-//                 Haroen Viaene <https://github.com/haroenv>
-//                 Samuel Vaillant <https://github.com/samouss>
-// Definitions: https://github.com/DefinitelyTyped/DefinitelyTyped
-// TypeScript Version: 2.8
 
 import { Client } from 'algoliasearch';
 import { EventEmitter } from 'events';
