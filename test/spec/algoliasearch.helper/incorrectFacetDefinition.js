b93bd36335c13359e8728f3524234fa1c6b92336
Haroen Viaene <hello@haroen.me>
2019-07-10 17:07:56 +0200

    test: remove wrong assertion

--- test/spec/algoliasearch.helper/incorrectFacetDefinition.js
+++ test/spec/algoliasearch.helper/incorrectFacetDefinition.js
@@ -19,7 +19,7 @@ test('Conjuctive facet should be declared to be excluded', function() {
 
   expect(_.bind(h.addExclude, h, 'undeclaredFacet', 'value')).toThrow();
   expect(_.bind(h.removeExclude, h, 'undeclaredFacet', 'value')).toThrow();
-  expect(_.bind(h.isExcluded, h, 'undeclaredFacet', 'value')).toThrow();
+  expect(h.isExcluded('undeclaredFacet', 'value')).toBe(false);
 });
 
 test('Conjuctive facet should be declared to be refine', function() {
