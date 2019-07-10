3627b70e3a46a98d5e09ebf92efd739e76e49bdd
Haroen Viaene <hello@haroen.me>
2019-07-10 16:12:13 +0200

    test: add test for no value

--- test/spec/SearchParameters/isFacetRefined.js
+++ test/spec/SearchParameters/isFacetRefined.js
@@ -13,6 +13,17 @@ test('isFacetRefined returns true if value in facetsRefinements', function() {
   expect(state.isFacetRefined('test', 'zongo')).toBe(true);
 });
 
+test('isFacetRefined returns true if something is refined when not passing a value', function() {
+  var state = new SearchParameters({
+    facets: ['test'],
+    facetsRefinements: {
+      test: ['zongo']
+    }
+  });
+
+  expect(state.isFacetRefined('test')).toBe(true);
+});
+
 test('isFacetRefined returns false if value not in facetsRefinements', function() {
   var state = new SearchParameters({
     facets: ['test'],
