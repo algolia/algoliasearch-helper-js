2341a8192ca9c80e637bda2326125ea883a504cc
Hugo Alliaume <kocal@live.Fr>
2020-04-02 12:38:23 +0200

    fix(defaultsPure): preserve keys order

--- src/functions/defaultsPure.js
+++ src/functions/defaultsPure.js
@@ -1,14 +1,30 @@
 'use strict';
 
 // NOTE: this behaves like lodash/defaults, but doesn't mutate the target
+// it also preserve keys order
 module.exports = function defaultsPure() {
   var sources = Array.prototype.slice.call(arguments);
-  return sources.reduceRight(function(acc, source) {
+  var mergedSources = sources.reduceRight(function(acc, source) {
     Object.keys(Object(source)).forEach(function(key) {
       if (source[key] !== undefined) {
         acc[key] = source[key];
       }
     });
+    return acc;
+  }, {});
+
+  var orderedKeys = sources.reduce(function(acc, source) {
+    Object.keys(Object(source)).forEach(function(key) {
+      if (!acc.includes(key)) {
+        acc.push(key);
+      }
+    });
+    return acc;
+  }, []);
+
+  return orderedKeys.reduce(function(acc, key) {
+    acc[key] = mergedSources[key];
+
     return acc;
   }, {});
 };
