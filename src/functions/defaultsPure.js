34507e06871c479afff41430f7b1b473733371a7
Hugo Alliaume <kocal@live.fr>
2020-04-20 22:57:04 +0200

    refactor(defaultsDeep): use @yannickcr's implementation

--- src/functions/defaultsPure.js
+++ src/functions/defaultsPure.js
@@ -4,22 +4,12 @@
 // it also preserve keys order
 module.exports = function defaultsPure() {
   var sources = Array.prototype.slice.call(arguments);
-  var mergedSources = sources.reduceRight(function(acc, source) {
+  return sources.reduce(function(acc, source) {
     Object.keys(Object(source)).forEach(function(key) {
-      if (source[key] !== undefined) {
+      if (source[key] !== undefined && !Object.hasOwnProperty.call(acc, key)) {
         acc[key] = source[key];
       }
     });
-    return acc;
-  }, {});
-
-  var orderedKeys = sources.reduce(function(acc, source) {
-    return acc.slice().concat(Object.keys(Object(source)));
-  }, []);
-
-  return orderedKeys.reduce(function(acc, key) {
-    acc[key] = mergedSources[key];
-
     return acc;
   }, {});
 };
