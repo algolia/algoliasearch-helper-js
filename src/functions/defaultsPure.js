b6131cc4d7ae04e4b95b9ab601c24c9bbaad9b7f
eunjae-lee <karis612@gmail.com>
2020-07-21 18:37:37 +0200

    fix(defaultsPure): fix the regression where the order was wrong with addFacetRefinement

--- src/functions/defaultsPure.js
+++ src/functions/defaultsPure.js
@@ -4,7 +4,22 @@
 // it also preserve keys order
 module.exports = function defaultsPure() {
   var sources = Array.prototype.slice.call(arguments);
-  return sources.reduce(function(acc, source) {
+
+  var keysInOrder = Object.keys(sources.reduceRight(function(acc, source) {
+    Object.keys(Object(source)).forEach(function(key) {
+      if (source[key] === undefined) {
+        return;
+      }
+      if (acc[key] === null) {
+        // remove if already added, so that we can add it in correct order
+        delete acc[key];
+      }
+      acc[key] = null;  // placeholder
+    });
+    return acc;
+  }, {}));
+
+  var merged = sources.reduce(function(acc, source) {
     Object.keys(Object(source)).forEach(function(key) {
       if (source[key] !== undefined && !Object.hasOwnProperty.call(acc, key)) {
         acc[key] = source[key];
@@ -12,4 +27,11 @@ module.exports = function defaultsPure() {
     });
     return acc;
   }, {});
+
+  var final = keysInOrder.reduce(function(acc, key) {
+    acc[key] = merged[key];
+    return acc;
+  }, {});
+
+  return final;
 };
