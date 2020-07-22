5a675cec7b773e2528bd564be5f259a3c5a2a1f9
eunjae-lee <karis612@gmail.com>
2020-07-22 12:25:36 +0200

    chore: improve performance

--- src/functions/defaultsPure.js
+++ src/functions/defaultsPure.js
@@ -2,36 +2,32 @@
 
 // NOTE: this behaves like lodash/defaults, but doesn't mutate the target
 // it also preserve keys order
+var PLACEHOLDER = '__PLACEHOLDER__';
+
 module.exports = function defaultsPure() {
   var sources = Array.prototype.slice.call(arguments);
 
-  var keysInOrder = Object.keys(sources.reduceRight(function(acc, source) {
+  var emptyObjectWithKeysInOrder = sources.reduceRight(function(acc, source) {
     Object.keys(Object(source)).forEach(function(key) {
       if (source[key] === undefined) {
         return;
       }
-      if (acc[key] === null) {
+      if (acc[key] === PLACEHOLDER) {
         // remove if already added, so that we can add it in correct order
         delete acc[key];
       }
-      acc[key] = null;  // placeholder
+      acc[key] = PLACEHOLDER;
     });
     return acc;
-  }, {}));
+  }, {});
 
-  var merged = sources.reduce(function(acc, source) {
+  return sources.reduce(function(acc, source) {
     Object.keys(Object(source)).forEach(function(key) {
-      if (source[key] !== undefined && !Object.hasOwnProperty.call(acc, key)) {
+      if (source[key] !== undefined && acc[key] === PLACEHOLDER) {
         acc[key] = source[key];
       }
     });
     return acc;
-  }, {});
-
-  var final = keysInOrder.reduce(function(acc, key) {
-    acc[key] = merged[key];
-    return acc;
-  }, {});
-
-  return final;
+  }, emptyObjectWithKeysInOrder);
 };
+
