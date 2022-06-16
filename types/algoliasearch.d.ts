3a6c53fc8760ad67dd2e35fbfe0c41e7b792af62
Haroen Viaene <hello@haroen.me>
2022-06-16 10:14:48 +0200

    simplification and correction when not in v5

--- types/algoliasearch.d.ts
+++ types/algoliasearch.d.ts
@@ -15,12 +15,15 @@ import type * as AlgoliaSearchFullV5 from '@experimental-api-clients-automation/
 // @ts-ignore
 import * as ClientSearchV5 from '@experimental-api-clients-automation/client-search';
 
+// turns any to unknown, so it can be used as a conditional
+type AnyToUnknown<T> = any extends T ? unknown : T;
+
 type DummySearchClientV4 = {
-  transporter: any;
+  transporter: unknown;
 };
 
-type DummySearchClientV5 = {
-  search: any;
+type DummySearchClient = {
+  search: unknown;
 };
 
 // @ts-ignore
@@ -30,18 +33,18 @@ type ClientLiteV5 = ReturnType<
 >;
 // @ts-ignore
 type ClientFullV5 = ReturnType<typeof AlgoliaSearchFullV5.algoliasearch>;
+type ClientV5 = AnyToUnknown<ClientLiteV5 extends DummySearchClient
+  ? ClientLiteV5
+  : ClientFullV5>;
 
 type PickForClient<
   T extends {
     v3: unknown;
     v4: unknown;
-    v5lite: unknown;
-    v5full: unknown;
+    v5: unknown;
   }
-> = ClientLiteV5 extends DummySearchClientV5
-  ? T['v5lite']
-  : ClientFullV5 extends DummySearchClientV5
-  ? T['v5full']
+> = ClientV5 extends DummySearchClient
+  ? T['v5']
   : ReturnType<typeof algoliasearch> extends DummySearchClientV4
   ? T['v4']
   : T['v3'];
@@ -52,9 +55,7 @@ type DefaultSearchClient = PickForClient<{
   // @ts-ignore
   v4: ReturnType<typeof algoliasearch>;
   // @ts-ignore
-  v5lite: ClientLiteV5;
-  // @ts-ignore
-  v5full: ClientFullV5;
+  v5: ClientV5;
 }>;
 
 export type SearchOptions = PickForClient<{
@@ -62,11 +63,7 @@ export type SearchOptions = PickForClient<{
   v3: AlgoliaSearchLite.QueryParameters;
   // @ts-ignore
   v4: ClientSearch.SearchOptions;
-  v5lite: NonNullable<
-    // @ts-ignore
-    ClientSearchV5.LegacySearchMethodProps[number]['params']
-  >;
-  v5full: NonNullable<
+  v5: NonNullable<
     // @ts-ignore
     ClientSearchV5.LegacySearchMethodProps[number]['params']
   >;
@@ -78,9 +75,7 @@ export type SearchResponse<T> = PickForClient<{
   // @ts-ignore
   v4: ClientSearch.SearchResponse<T>;
   // @ts-ignore
-  v5lite: ClientSearchV5.SearchResponse; // TODO: should be generic
-  // @ts-ignore
-  v5full: ClientSearchV5.SearchResponse;
+  v5: ClientSearchV5.SearchResponse; // TODO: should be generic
 }>;
 
 export type SearchForFacetValuesResponse = PickForClient<{
@@ -89,25 +84,21 @@ export type SearchForFacetValuesResponse = PickForClient<{
   // @ts-ignore
   v4: ClientSearch.SearchForFacetValuesResponse;
   // @ts-ignore
-  v5lite: ClientSearchV5.SearchForFacetValuesResponse;
-  // @ts-ignore
-  v5full: ClientSearchV5.SearchForFacetValuesResponse;
+  v5: ClientSearchV5.SearchForFacetValuesResponse;
 }>;
 
 export type FindAnswersOptions = PickForClient<{
   v3: any;
   // @ts-ignore
   v4: ClientSearch.FindAnswersOptions;
-  v5lite: any;
-  v5full: any;
+  v5: any;
 }>;
 
 export type FindAnswersResponse<T> = PickForClient<{
   v3: any;
   // @ts-ignore
   v4: ClientSearch.FindAnswersResponse<T>;
-  v5lite: any;
-  v5full: any;
+  v5: any;
 }>;
 
 export interface SearchClient {
