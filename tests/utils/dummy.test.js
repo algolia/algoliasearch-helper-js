df6630cf79acb11287bd7eca6f7a6ca6f9e496e0
Dhaya <154633+dhayab@users.noreply.github.com>
2023-06-22 19:00:09 +0200

    make tests pass from root

---
+++ tests/utils/dummy.test.js
@@ -0,0 +1,5 @@
+describe('dummy test to pass jest', () => {
+  it('passes', () => {
+    expect(true).toBeTruthy();
+  });
+});
---
+++ tests/utils/setupTests.ts
@@ -0,0 +1,2 @@
+// Dummy file to make Jest tests pass.
+// This will create a conflict during merge, Incoming content should be kept.
